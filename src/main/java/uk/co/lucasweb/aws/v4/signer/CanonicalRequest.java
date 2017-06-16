6cb33a5a56bf0cc5a211a0b50a58625f05ac8689
Yoann Rodière <yoann@hibernate.org>
2017-06-16 15:35:25 +0200

    Sort query parameters before hashing them
    This fixes test 'get-vanilla-query-order-key-case' from the AWS test suite.

--- src/main/java/uk/co/lucasweb/aws/v4/signer/CanonicalRequest.java
+++ src/main/java/uk/co/lucasweb/aws/v4/signer/CanonicalRequest.java
@@ -15,12 +15,18 @@ package uk.co.lucasweb.aws.v4.signer;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.co.lucasweb.aws.v4.signer.encoding.URLEncoding;
 
 /**
  * @author Richard Lucas
  */
 class CanonicalRequest {
+
+    private static final char QUERY_PARAMETER_SEPARATOR = '&';
+    private static final char QUERY_PARAMETER_VALUE_SEPARATOR = '=';
 
     private final HttpRequest httpRequest;
     private final CanonicalHeaders headers;
@@ -35,7 +41,7 @@ class CanonicalRequest {
     String get() {
         return httpRequest.getMethod() +
                 "\n" + normalizePath(httpRequest.getPath()) +
-                "\n" + httpRequest.getQuery() +
+                "\n" + normalizeQuery(httpRequest.getQuery()) +
                 "\n" + headers.get() +
                 "\n" + headers.getNames() +
                 "\n" + contentSha256;
@@ -62,6 +68,99 @@ class CanonicalRequest {
                     "The encoded path '" + path + "' was deemed syntactically incorrect;"
                     + " there is probably an internal issue with the encoding algorithm"
             );
+        }
+    }
+
+    private static String normalizeQuery(String rawQuery) {
+        if (rawQuery == null || rawQuery.isEmpty()) {
+            return "";
+        }
+
+        List<Parameter> parameters = extractQueryParameters(rawQuery);
+
+        /*
+         * Sort query parameters. Simply sort lexicographically by character
+         * code, which is equivalent to comparing code points (as mandated by
+         * AWS)
+         */
+        parameters.sort((l, r) -> l.name.compareTo(r.name));
+
+        StringBuilder builder = new StringBuilder();
+        boolean first = true;
+        for (Parameter parameter : parameters) {
+            if (first) {
+                first = false;
+            } else {
+                builder.append(QUERY_PARAMETER_SEPARATOR);
+            }
+            String name = parameter.name;
+            String value = parameter.value;
+            if (value == null) {
+                // No value => use an empty string as per the spec
+                value = "";
+            }
+            builder.append(name).append(QUERY_PARAMETER_VALUE_SEPARATOR).append(value);
+        }
+
+        return builder.toString();
+    }
+
+    /**
+     * Extract parameters from a query string, preserving encoding.
+     * <p>
+     * We can't use Apache HTTP Client's URLEncodedUtils.parse, mainly because
+     * we don't want to decode names/values.
+     *
+     * @param rawQuery
+     *            the query to parse
+     * @return The list of parameters, in the order they were found.
+     */
+    private static List<Parameter> extractQueryParameters(String rawQuery) {
+        List<Parameter> results = new ArrayList<>();
+        int endIndex = rawQuery.length() - 1;
+        int index = 0;
+        while (0 <= index && index <= endIndex) {
+            /*
+             * Ideally we should first look for '&', then look for '=' before
+             * the '&', but obviously that's not how AWS understand query
+             * parsing; see the test "post-vanilla-query-nonunreserved" in the
+             * test suite. A string such as "?foo&bar=qux" will be understood as
+             * one parameter with name "foo&bar" and value "qux". Don't ask me
+             * why.
+             */
+            String name;
+            String value;
+            int nameValueSeparatorIndex = rawQuery.indexOf(QUERY_PARAMETER_VALUE_SEPARATOR, index);
+            if (nameValueSeparatorIndex < 0) {
+                // No value
+                name = rawQuery.substring(index);
+                value = null;
+
+                index = endIndex + 1;
+            } else {
+                int parameterSeparatorIndex = rawQuery.indexOf(QUERY_PARAMETER_SEPARATOR, nameValueSeparatorIndex);
+                if (parameterSeparatorIndex < 0) {
+                    parameterSeparatorIndex = endIndex + 1;
+                }
+                name = rawQuery.substring(index, nameValueSeparatorIndex);
+                value = rawQuery.substring(nameValueSeparatorIndex + 1, parameterSeparatorIndex);
+
+                index = parameterSeparatorIndex + 1;
+            }
+
+            results.add(new Parameter(name, value));
+        }
+        return results;
+    }
+
+    private static final class Parameter {
+        private final String name;
+        private final String value;
+
+        public Parameter(String name, String value) {
+            super();
+            this.name = name;
+            this.value = value;
         }
     }
 }
